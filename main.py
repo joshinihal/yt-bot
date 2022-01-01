c5e37702a2294913cabdfaa5b2a0d470fea6a24a
Nihal <nihal@mobiuso.com>
2022-01-01 14:00:17 +0530

    add procfile

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from selenium.webdriver.support import expected_conditions as EC
 import os
 import time
 
-channel_url = ''
+channel_url = 'https://www.youtube.com/channel/UC_0IIMmTZvnfO9ucN87qUhQ/videos'
 
 while True:
     options = webdriver.ChromeOptions()
